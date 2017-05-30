15e79b88af80793ee604e6c38bbe505309d42101
yunweiwo <195343918@qq.com>
2017-05-30 23:17:43 +0800

    测试123

--- src/com/test/springboot/Application.java
+++ src/com/test/springboot/Application.java
@@ -12,7 +12,7 @@ public class Application
 
     @RequestMapping("/")
     public String greeting() {
-        return "New Hello World";
+        return "Hello World!";
     }
 
     public static void main(String[] args) {
